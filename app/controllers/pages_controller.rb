1829aee75dd48b1058607c7efa8c839e888a3321
beth geast <beth.geast@gmail.com>
2024-08-20 20:40:33 +0100

    routes, views, controller actions generated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+    # @hobbies = ["Dogs", "Video Gaming", "Music (Listening, Live & Performing)", "Puzzles"]
+  end
+
+  def contact
+  end
+
+  def resume
+    @techstack = ["Ruby", "Ruby On Rails", "HTML5", "CSS/SCSS", "Javascript", "Git", "Github", "TDD", "Z-Shell", "Visual Studio Code", "Ubuntu"]
+    @other = ["Python", "SQL & PostgreSQL", "Heroku", "Stimulus", "Bootstrap", "Redis", "Cloudinary", "Action Cable", "Websocket", "Figma"]
+    @learning = ["React", "Redux", "Webpack"]
+    @additional = ["Full Clean UK Driving Licence", "Microsoft Office"]
+    @strengths = ["Trustworthy & Reliable", "Empathetic & Approachable", "Fast Learner", "Dedicated", "Strong Listener", "Team Player", "Organisation"]
+  end
+
+  def projects
+  end
 end
