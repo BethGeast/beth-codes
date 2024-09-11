1af777778bad897c87dd0be4ce45ec7f4bd961d7
beth geast <beth.geast@gmail.com>
2024-09-11 23:29:16 +0100

    checkpoint, one carousel and modal functional, ongoing styling tweaks

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+import { Modal } from "bootstrap"
+
+// Connects to data-controller="carousel"
+export default class extends Controller {
+  static targets = ["image", "modal", "modalCarousel", "modalItem"];
+
+  connect() {
+    this.initializeCarousel();
+  }
+
+  initializeCarousel() {
+    this.imageTargets.forEach((img, index) => {
+      img.addEventListener("click", () => this.openModal(index));
+    });
+  }
+
+  openModal(index) {
+    const modalCarousel = new bootstrap.Carousel(this.modalCarouselTarget);
+    this.modalItemTargets.forEach((item, idx) => item.classList.toggle("active", idx === index));
+    new Modal(this.modalTarget).show();
+    modalCarousel.to(index);
+  }
+}
