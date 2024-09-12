62cca49ec70823ed51184d63e6398a5b987a3e3f
beth geast <beth.geast@gmail.com>
2024-09-12 22:39:17 +0100

    first carousel implemented and fully working without bugs

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -7,6 +7,7 @@ export default class extends Controller {
 
   connect() {
     this.initializeCarousel();
+    this.syncCarousels();
   }
 
   initializeCarousel() {
@@ -18,7 +19,33 @@ export default class extends Controller {
   openModal(index) {
     const modalCarousel = new bootstrap.Carousel(this.modalCarouselTarget);
     this.modalItemTargets.forEach((item, idx) => item.classList.toggle("active", idx === index));
-    new Modal(this.modalTarget).show();
+    new bootstrap.Modal(this.modalTarget).show();
     modalCarousel.to(index);
+  }
+
+  syncCarousels() {
+    this.modalTarget.addEventListener("hidden.bs.modal", () => {
+      this.cleanupModal();
+      const activeIndex = Array.from(this.modalItemTargets).findIndex((item) =>
+        item.classList.contains("active")
+      );
+      const mainCarousel = new bootstrap.Carousel(document.getElementById("gigbuddyCarousel")
+      );
+      mainCarousel.to(activeIndex);
+    });
+    this.modalCarouselTarget.addEventListener("slide.bs.carousel", event => {
+      const mainCarousel = new bootstrap.Carousel(
+        document.getElementById("gigbuddyCarousel")
+      );
+      mainCarousel.to(event.to);
+    });
+  }
+
+  cleanupModal() {
+    const backdrops = document.querySelectorAll(".modal-backdrop");
+    backdrops.forEach((backdrop) => backdrop.remove());
+    document.body.classList.remove("modal-open");
+    document.body.style.removeProperty("overflow");
+    document.body.style.removeProperty("padding-right");
   }
 }
